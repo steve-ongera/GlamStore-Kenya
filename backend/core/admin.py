23cf247a37c3dd168f5df4b7f16ca8944c25e428
steve-ongera <gadafisteve001@gmail.com>
2026-04-20 21:02:51 +0300

    made updates

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,26 @@
+# core/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User, Address
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['email', 'first_name', 'last_name', 'phone', 'is_active', 'is_staff', 'date_joined']
+    list_filter = ['is_active', 'is_staff']
+    search_fields = ['email', 'first_name', 'last_name', 'phone']
+    ordering = ['-date_joined']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'phone', 'preferred_county')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {'classes': ('wide',), 'fields': ('email', 'first_name', 'last_name', 'password1', 'password2')}),
+    )
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'label', 'town', 'county', 'is_default']
