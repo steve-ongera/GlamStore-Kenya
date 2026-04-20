b7f89e68f025877e6b8134d11a7f8a6ef104b1ae
steve-ongera <gadafisteve001@gmail.com>
2026-04-20 21:02:23 +0300

    made updates

--- backend/pickups/admin.py
+++ backend/pickups/admin.py
@@ -1,3 +1,24 @@
+# pickups/admin.py
 from django.contrib import admin
+from .models import County, PickupStation
 
-# Register your models here.
+
+class PickupStationInline(admin.TabularInline):
+    model = PickupStation
+    extra = 1
+
+
+@admin.register(County)
+class CountyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code', 'delivery_fee', 'is_active']
+    list_editable = ['delivery_fee', 'is_active']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [PickupStationInline]
+
+
+@admin.register(PickupStation)
+class PickupStationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'county', 'town', 'pickup_fee', 'is_active']
+    list_filter = ['county', 'is_active']
+    list_editable = ['pickup_fee', 'is_active']
+    search_fields = ['name', 'address', 'town']
